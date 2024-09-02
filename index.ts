3e346ca5857895490a0c811fc101524d3c507adb
bittere <109094899+bittere@users.noreply.github.com>
2024-09-02 01:57:37 +0000

    use streams for input/output instead of default stdin and stdout

--- index.ts
+++ index.ts
@@ -3,8 +3,44 @@ import type {
 	DashCommand,
 	DashOpts,
 	DashState,
+	DashStreamInterface,
 	DashWrapper,
 } from "./types.ts";
+
+/**
+ * Creates a `DashStreamInterface` from a given readable and writable stream.
+ *
+ * A `DashStreamInterface` is an object with a `question` method and a `log` method.
+ * The `question` method takes a string, encodes it with a TextEncoder, writes it to the given writable stream, waits for a string to be written to the given readable stream, decodes it with a TextDecoder, and returns the decoded string.
+ * The `log` method takes a string, encodes it with the given encoder, and writes it to the given writable stream.
+ *
+ * @param {ReadableStream} input The readable stream to read from.
+ * @param {WritableStream} output The writable stream to write to.
+ *
+ * @returns {DashStreamInterface} The `DashStreamInterface` object.
+ */
+function createInterface(
+	input: ReadableStream,
+	output: WritableStream
+): DashStreamInterface {
+	const reader = input.getReader();
+	const writer = output.getWriter();
+
+	const encoder = new TextEncoder();
+	const decoder = new TextDecoder("utf-8");
+
+	return {
+		question: async (line: string) => {
+			// a writable stream to avoid the newline when using console.log
+			writer.write(encoder.encode(line));
+			const op = await reader.read();
+			return decoder.decode(op.value).trim();
+		},
+		log: (line: string) => {
+			writer.write(encoder.encode(line));
+		},
+	};
+}
 
 /**
  * Creates a `dash` wrapper.
@@ -13,9 +49,16 @@ import type {
  */
 function dash(opts?: DashOpts): DashWrapper {
 	const commands = new Map<string, DashCommand>();
-	let state: DashState = opts?.initState || {
-		dir: "~",
-	};
+	let state =
+		opts?.initState ||
+		({
+			dir: "~",
+		} as DashState);
+
+	const rw = createInterface(
+		opts?.stdin ?? Deno.stdin.readable,
+		opts?.stdout ?? Deno.stdout.writable
+	);
 
 	return {
 		/**
@@ -30,17 +73,17 @@ function dash(opts?: DashOpts): DashWrapper {
 		/**
 		 * Starts the `dash` shell.
 		 * This function will not return until the user ends the input/program (eg. with Ctrl+D, Ctrl+C or with the built-in `exit` command.)
-		 * @returns {void}
+		 * @returns {Promise<void>}
 		 */
-		start: (): void => {
+		start: async (): Promise<void> => {
 			if (opts?.initMessage) {
-				console.log(opts?.initMessage);
+				rw.log(opts?.initMessage);
 			}
 
 			while (true) {
-				console.log();
-				const userCommand = globalThis.prompt(
-					opts?.prompt?.(state) ?? ">"
+				rw.log("\n");
+				const userCommand = await rw.question(
+					opts?.prompt?.(state) ?? "> "
 				);
 
 				if (!userCommand) {
@@ -54,19 +97,18 @@ function dash(opts?: DashOpts): DashWrapper {
 					break;
 				}
 
-				if (commandName === "clear") {
-					console.clear();
-					continue;
-				}
-
 				const args = parseArgs(commandChunks);
 				args.__ = commandChunks;
 
 				if (commands.get(commandName)) {
+					// why are we passing the `rw.log` function here?
+					// when testing, it is useful to send the data to the `stdout` initialzation option instead of `stdout` of the process
+					// console.log still totally works though tests may fail
+
 					// biome-ignore lint/style/noNonNullAssertion: we're checking that the command exists in the above if block
-					state = commands.get(commandName)!(args, state) || state;
+					state = commands.get(commandName)!(args, state, rw.log) || state;
 				} else {
-					console.log(`dash: command not found: ${commandName}`);
+					rw.log(`dash: command not found: ${commandName}`);
 				}
 			}
 		},
--- types.ts
+++ types.ts
@@ -8,15 +8,32 @@ type DashState = {
 interface DashOpts {
 	initMessage?: string;
 	initState?: DashState;
+	stdin?: ReadableStream;
+	stdout?: WritableStream;
 	prompt?: (state: DashState) => string;
 }
 
-// biome-ignore lint/suspicious/noConfusingVoidType: functions without return values are typically typed as void
-type DashCommand = (options: Args, state: DashState) => DashState | void;
+type DashCommand = (
+	options: Args,
+	state: DashState,
+	log: DashStreamInterface["log"]
+	// biome-ignore lint/suspicious/noConfusingVoidType: functions without return values are typically typed as void
+) => DashState | void;
 
 interface DashWrapper {
-	start: () => void;
+	start: () => Promise<void>;
 	register: (command: string, fn: DashCommand) => void;
 }
 
-export type { DashOpts, DashState, DashCommand, DashWrapper };
+interface DashStreamInterface {
+	question: (line: string) => Promise<string>;
+	log: (line: string) => void;
+}
+
+export type {
+	DashOpts,
+	DashState,
+	DashCommand,
+	DashWrapper,
+	DashStreamInterface,
+};
